9e4246231004ee91396677c72a5290f8facb9587
Emiliano Aceves Hernández <0223332@up.edu.mx>
2022-05-19 03:00:33 -0500

    First version of questioncontainer
    Not yet done, but can be used for testings

---
+++ components/QuestionContainer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { View, StyleSheet, Button } from "react-native";
+
+const QuestionContainer = ({children, style}) => {
+  return <View style={[styles.questionContainer, style]}>{children}
+   
+  </View>
+}
+
+const styles = StyleSheet.create({
+    questionContainer: {
+        width: 300,
+        margin: 10,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: .26,
+        shadowRadius: 6,
+        backgroundColor: 'white',
+        elevation: 5, //Only Android
+        padding: 20,
+        borderRadius: 10
+    },
+})
+
+export default QuestionContainer;

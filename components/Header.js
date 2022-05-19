c3ef29b7da690656c35480a98efffd282f098551
Emiliano Aceves Hernández <0223332@up.edu.mx>
2022-05-19 03:37:22 -0500

    Header in b&W
    Not final version

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { StyleSheet,  Text, View} from 'react-native'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <View style={styles.header}>
+      <Text>Waifu Test</Text>
+    </View>
   )
 }
-
+const styles = StyleSheet.create({
+  header: {
+      width: '100%',
+      height: 70,
+      backgroundColor: 'black',
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  headerTitle: {
+      color: 'white',
+      fontSize: 35,
+  }
+});
 export default Header

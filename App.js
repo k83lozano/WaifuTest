68897675902176f9ce06ec4705a88b230329fba6
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2022-05-19 02:06:51 -0500

    Imported screens into App.js
    Imported QuizScreen.js, ResultScreen.js and StartQuizScreen.js into App.js and created a variable which will render a screen dynamically.

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import QuizScreen from './screens/QuizScreen';
+import ResultScreen from './screens/ResultScreen';
+import StartQuizScreen from './screens/StartQuizScreen';
 
 export default function App() {
+
+  let content = <StartQuizScreen></StartQuizScreen>
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {content}
     </View>
   );
 }

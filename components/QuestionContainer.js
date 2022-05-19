d986df2635f456f4e02e00fa65ed4715d075cfa3
Emiliano Aceves Hernández <0223332@up.edu.mx>
2022-05-19 15:35:25 -0500

    Merge branch 'QuestionContainer' of https://github.com/GranOla18/WaifuTest into QuestionContainer
    Changes on QuestionContainer to make it only a component and nothing more

--- components/QuestionContainer.js
+++ components/QuestionContainer.js
@@ -1,26 +1,9 @@
 import React, {useEffect, useState} from 'react'
 import { View, StyleSheet, Button } from "react-native";
 
-const QuestionContainer = (props) => {
-  const [question, setQuestion]=useState({ques:'', ans1:'', ans2:'', ans3:'', ans4:''})
-
-  useEffect(()=>{
-    if (props.children !=0){
-      setQuestion()
-    }
-  }, [props.children])
-
-  setQuestion=async()=>{
-    const[ques, ans1, ans2, ans3, ans4]=await fetchQuestion(props.children)
-    setQuestion({ques: ques, ans1: ans1, ans2: ans2, ans3: ans3, ans4: ans4})
-  }
-
+const QuestionContainer = ({children}) => {
   return <View style={styles.questionContainer}>
-    <Text style={styles.question}>{props.ques}</Text>
-    <Button style={styles.answer} title={props.ans1}/>
-    <Button style={styles.answer} title={props.ans2}/>
-    <Button style={styles.answer} title={props.ans3}/>
-    <Button style={styles.answer} title={props.ans3}/>
+    {children}
   </View>
 }
 
